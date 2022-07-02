7aecabdea04136b3fc0ad9b79fe39a4937a8438b
TONNYCHWEYA <chweyatonny@gmail.com>
2022-07-02 10:59:00 +0300

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -5,15 +5,20 @@ const plus = document.getElementById("plus")
 const heart = document.getElementById("heart")
 const likes = document.getElementsByClassName("likes")[0]
 const pause = document.getElementById("pause")
-document.addEventListener("DOMContentLoaded", (event) =>{
- 
-  console.log(counterValue)
-  setInterval(incrementNo, 1000)
-  function incrementNo(){
+
+document.addEventListener("DOMContentLoaded", handleDocumentEvent)
+
+function incrementNo(){
   counterValue++;
   counter.textContent = counterValue
 }
-})
+
+function handleDocumentEvent(event){
+    myInterval
+}
+
+  let myInterval = setInterval(incrementNo, 1000)
+
 
 minus.addEventListener("click", (event)=>{
    counterValue--
@@ -39,7 +44,12 @@ heart.addEventListener("click", (event) => {
 
   pause.addEventListener("click", handleClickEvent)
 
-
+function setCounterInterval(){
+  setInterval(incrementNo, 1000)
+}
+function restart(){
+setTimeout(setCounterInterval, 3000)
+}
 
 function handleClickEvent(event){
   let pauseValue = pause.textContent
@@ -48,12 +58,28 @@ function handleClickEvent(event){
     minus.disabled = true
     plus.disabled = true
     heart.disabled = true
+    
+    clearInterval(myInterval)
     pause.textContent = "Resume"
 }else{
   minus.disabled = false
     plus.disabled = false
     heart.disabled = false
+    restart()
+
     pause.textContent = "pause"
 }
+}
+
+
+const form = document.getElementById("comment-form")
+form.addEventListener("submit", handleFormEvent)
+function handleFormEvent(event){
+  event.preventDefault()
+  const inputValue = event.target.input.value 
+  let p = document.createElement("p")
+  p.textContent = inputValue
+  let div = document.getElementById("list")
+  div.appendChild(p)
 }
 
